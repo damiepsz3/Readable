7078a6df2339485bae3e0c7a6b1e3da8abf7af7c
damienepsztein <damiepsz3@gmail.com>
2017-10-23 16:12:12 -0400

    feat: ✨ go back feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router'
 import '../App.css'
 import MainContainer from '../containers/MainContainer/MainContainer'
 class App extends Component {
-
   render() {
+    const route = this.props;
     return (
       <div className="blog">
         <div className="blog-title">
           <h1>Readable Blog</h1>
+          {route.location.pathname !== '/' &&
+            <a className="go-back" onClick={route.history.goBack}>Go back</a>
+          }
         </div>
         <MainContainer/>
       </div>
@@ -15,4 +19,4 @@ class App extends Component {
   }
 }
 
-export default App
+export default withRouter(App)
