27220bc8d33496a8717f41da2e8cb0b7d50f7be3
damiepsz3 <damiepsz3@gmail.com>
2017-11-04 20:19:46 -0400

    refactor: clenaning up code

--- client/src/reducers/uistate/index.js
+++ client/src/reducers/uistate/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
 import sortBy from './sortReducer'
-import error from './errorReducer'
 import modal from './modalReducer'
 
 export default combineReducers({
   sortBy,
-  error,
   modal
 })
